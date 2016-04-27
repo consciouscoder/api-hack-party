7f4a1caecc0612a78d78dfb24ff7bade1e3948c9
dladendorf@gmail.com <dladendorf@gmail.com>
2016-04-27 22:48:59 +0000

    star wars planets API - 3 planet

--- main.js
+++ main.js
@@ -13,16 +13,120 @@
         
         // jCtrl.searchJedi = function() {
             
-            $http.get('https://swapi.co/api/planets/1')
+            $http.get('https://swapi.co/api/planets/1/')
                 .then(function(response){
  
-                   // jCtrl.jedi = response.data.results
+                    jCtrl.tatooine = response.data
                     
+                   // jCtrl.jedi = response.data.results
                     // console.log(response.data.results)
                     
-                    console.log(response)
+                    console.log(jCtrl.tatooine)
 
                 })   
+                
+             $http.get('https://swapi.co/api/planets/2/')
+                .then(function(response){
+ 
+                    jCtrl.alderaan = response.data
+                    
+                   // jCtrl.jedi = response.data.results
+                    // console.log(response.data.results)
+                    
+                    console.log(jCtrl.alderaan)
+
+                })   
+                
+            $http.get('https://swapi.co/api/planets/8/')
+                .then(function(response){
+ 
+                    jCtrl.naboo = response.data
+                    
+                   // jCtrl.jedi = response.data.results
+                    // console.log(response.data.results)
+                    
+                    console.log(jCtrl.naboo)
+
+                })            
+                
+            jCtrl.tatooineShow = function() {
+                //console.log('Showing Tatooine')
+                var tatooineStr = ""
+                tatooineStr = "\nName: " + jCtrl.tatooine.name +
+                            "\nPopulation: " + jCtrl.tatooine.population +
+                            "\nTerrain: " + jCtrl.tatooine.terrain +
+                            "\nClimate: " + jCtrl.tatooine.climate +
+                            "\nDiameter: " + jCtrl.tatooine.diameter +
+                            "\nOrbital Period: " + jCtrl.tatooine.orbital_period
+                            "\nRotation Period: " + jCtrl.tatooine.rotation_period
+                            
+                jCtrl.tatooineStr = tatooineStr
+                
+                jCtrl.planetStr = tatooineStr
+                
+                jCtrl.planetName = jCtrl.tatooine.name
+                jCtrl.planetPop = jCtrl.tatooine.population
+                jCtrl.planetTerrain = jCtrl.tatooine.terrain
+                jCtrl.planetClimate  = jCtrl.tatooine.climate
+                jCtrl.planetDiameter = jCtrl.tatooine.diameter
+                jCtrl.planetOrbital = jCtrl.tatooine.orbital_period     
+                jCtrl.planetGravity = jCtrl.tatooine.gravity              
+                            
+                console.log(jCtrl.tatooineStr)
+            }
+            
+            jCtrl.alderaanShow = function() {
+                //console.log('Showing Alderaan')
+                var alderaanStr = ""
+                alderaanStr = "\nName: " + jCtrl.alderaan.name +
+                            "\nPopulation: " + jCtrl.alderaan.population +
+                            "\nTerrain: " + jCtrl.alderaan.terrain +
+                            "\nnClimate: " + jCtrl.alderaan.climate +
+                            "\nDiameter: " + jCtrl.alderaan.diameter +
+                            "\nPeriod: " + jCtrl.alderaan.orbital_period
+                            "\nRotation Period: " + jCtrl.alderaan.rotation_period          
+                
+                jCtrl.alderaanStr = alderaanStr
+                
+                jCtrl.planetStr = alderaanStr
+                
+                jCtrl.planetName = jCtrl.alderaan.name
+                jCtrl.planetPop = jCtrl.alderaan.population
+                jCtrl.planetTerrain = jCtrl.alderaan.terrain
+                jCtrl.planetClimate  = jCtrl.alderaan.climate
+                jCtrl.planetDiameter = jCtrl.alderaan.diameter
+                jCtrl.planetOrbital = jCtrl.alderaan.orbital_period  
+                jCtrl.planetGravity = jCtrl.alderaan.gravity
+                
+                console.log(jCtrl.alderaanStr)    
+                
+            }
+            
+            jCtrl.nabooShow = function() {
+               //console.log('Showing Naboo')
+                var nabooStr = ""
+                nabooStr = "\nName: " + jCtrl.naboo.name +
+                            "\nPopulation: " + jCtrl.naboo.population +
+                            "\nTerrain: " + jCtrl.naboo.terrain +
+                            "\nClimate: " + jCtrl.naboo.climate +
+                            "\nDiameter: " + jCtrl.naboo.diameter +
+                            "\nOrbital Period: " + jCtrl.naboo.orbital_period
+                            "\nRotation Period: " + jCtrl.naboo.rotation_period     
+                    
+                jCtrl.nabooStr = nabooStr
+                
+                jCtrl.planetStr = nabooStr
+                
+                 jCtrl.planetName = jCtrl.naboo.name
+                jCtrl.planetPop = jCtrl.naboo.population
+                jCtrl.planetTerrain = jCtrl.naboo.terrain
+                jCtrl.planetClimate  = jCtrl.naboo.climate
+                jCtrl.planetDiameter = jCtrl.naboo.diameter
+                jCtrl.planetOrbital = jCtrl.naboo.orbital_period     
+                jCtrl.planetGravity = jCtrl.naboo.gravity
+                
+                console.log(jCtrl.nabooStr) 
+            }            
                 
             // $http.get('https://swapi.co/api/people/')
             //     .then(function(response){
